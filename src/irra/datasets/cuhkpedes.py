6367d7f44fbb2b7bd4e7278f4627ec95ce0e1c61
AwePhD <mathias.reus@gipsa-lab.fr>
2023-05-17 08:55:35 +0000

    feat: restrict datasets to sysu annotations

--- src/irra/datasets/cuhkpedes.py
+++ src/irra/datasets/cuhkpedes.py
@@ -38,16 +38,17 @@ class CUHKPEDES(BaseDataset):
         self.anno_path = op.join(self.dataset_dir, 'reid_raw.json')
         self._check_before_run()
 
-        self.train_annos, self.test_annos, self.val_annos = self._split_anno(self.anno_path)
+        self.train_annos, self.test_annos, self.val_annos = self._split_anno(
+            self.anno_path)
 
-        self.train, self.train_id_container = self._process_anno(self.train_annos, training=True)
+        self.train, self.train_id_container = self._process_anno(
+            self.train_annos, training=True)
         self.test, self.test_id_container = self._process_anno(self.test_annos)
         self.val, self.val_id_container = self._process_anno(self.val_annos)
 
         if verbose:
             self.logger.info("=> CUHK-PEDES Images and Captions are loaded")
             self.show_dataset_info()
-
 
     def _split_anno(self, anno_path: str):
         train_annos, test_annos, val_annos = [], [], []
@@ -61,23 +62,27 @@ class CUHKPEDES(BaseDataset):
                 val_annos.append(anno)
         return train_annos, test_annos, val_annos
 
-
     def _process_anno(self, annos: List[dict], training=False):
         pid_container = set()
+
+        sysu_annos = [
+            anno
+            for anno in annos
+            if 'query' in anno['file_path']
+        ]
+
         if training:
             dataset = []
             image_id = 0
-            for anno in annos:
-                pid = int(anno['id']) - 1 # make pid begin from 0
+
+            for anno in sysu_annos:
+                pid = int(anno['id']) - 1  # make pid begin from 0
                 pid_container.add(pid)
                 img_path = op.join(self.img_dir, anno['file_path'])
-                captions = anno['captions'] # caption list
+                captions = anno['captions']  # caption list
                 for caption in captions:
                     dataset.append((pid, image_id, img_path, caption))
                 image_id += 1
-            for idx, pid in enumerate(pid_container):
-                # check pid begin from 0 and no break
-                assert idx == pid, f"idx: {idx} and pid: {pid} are not match"
             return dataset, pid_container
         else:
             dataset = {}
@@ -85,13 +90,13 @@ class CUHKPEDES(BaseDataset):
             captions = []
             image_pids = []
             caption_pids = []
-            for anno in annos:
+            for anno in sysu_annos:
                 pid = int(anno['id'])
                 pid_container.add(pid)
                 img_path = op.join(self.img_dir, anno['file_path'])
                 img_paths.append(img_path)
                 image_pids.append(pid)
-                caption_list = anno['captions'] # caption list
+                caption_list = anno['captions']  # caption list
                 for caption in caption_list:
                     captions.append(caption)
                     caption_pids.append(pid)
@@ -103,11 +108,11 @@ class CUHKPEDES(BaseDataset):
             }
             return dataset, pid_container
 
-
     def _check_before_run(self):
         """Check if all files are available before going deeper"""
         if not op.exists(self.dataset_dir):
-            raise RuntimeError("'{}' is not available".format(self.dataset_dir))
+            raise RuntimeError(
+                "'{}' is not available".format(self.dataset_dir))
         if not op.exists(self.img_dir):
             raise RuntimeError("'{}' is not available".format(self.img_dir))
         if not op.exists(self.anno_path):
